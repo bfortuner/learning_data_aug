39dce2970091b411b4f775eb9cd44c65d392606d
Brendan Fortuner <bfortuner@gmail.com>
2018-03-02 23:10:09 -0800

    Updated models and added ability ot inject VAE augmentation into CNN training loop

--- datasets.py
+++ datasets.py
@@ -1,4 +1,5 @@
 import torch
+from torch.autograd import Variable
 import torchvision
 from torchvision import transforms
 
@@ -8,27 +9,43 @@ CIFAR_CLASSES = (
     'dog', 'frog', 'horse', 'ship', 'truck'
 )
 
-def get_cifar_loader(bs=64, trn_size=50000, tst_size=10000):
-    trainTransform = transforms.Compose([
-        #transforms.RandomHorizontalFlip(),
-        transforms.ToTensor()
-    ])
-    testTransform = transforms.Compose([
-        transforms.ToTensor()
-    ])
+TRN_TRANSFORM = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor()
+])
+TST_TRANSFORM = transforms.Compose([
+    transforms.ToTensor()
+])
+
+class LearnedTransform():
+    def __init__(self, model):
+        self.model = model
+
+    def __call__(self, x):
+        x = Variable(x)
+        return self.model.transform(x)
+
+
+def get_cifar_dataset(trn_size=50000, tst_size=10000,
+                      trn_transform=TRN_TRANSFORM, tst_transform=TST_TRANSFORM):
 
     trainset = torchvision.datasets.CIFAR10(
-        root='data/', train=True, download=True, transform=trainTransform)
+        root='data/', train=True, download=True, transform=trn_transform)
     trainset.train_data = trainset.train_data[:trn_size]
     trainset.train_labels = trainset.train_labels[:trn_size]
-    trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=bs, shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(
-        root='data/', train=False, download=True, transform=testTransform)
+        root='data/', train=False, download=True, transform=tst_transform)
     testset.test_data = testset.test_data[:tst_size]
     testset.test_labels = testset.test_labels[:tst_size]
+
+    return trainset, testset
+
+
+def get_cifar_loader(trainset, testset, batch_size=64):
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=2)
     testloader = torch.utils.data.DataLoader(
-        testset, batch_size=bs, shuffle=False, num_workers=2)
+        testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
     return trainloader, testloader
